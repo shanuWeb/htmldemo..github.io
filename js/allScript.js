db25e27124f73485903936c29260b608617c0cb7
shanuWeb <shanusanyalweb@gmail.com>
2020-03-06 01:19:25 +0530

    Footer and background change

--- js/allScript.js
+++ js/allScript.js
@@ -11,6 +11,33 @@ jQuery(window).load(function(){
 	  	jQuery('body').toggleClass('overflow');
 	});
 });
+
+$(window).scroll(function(){
+  var sticky = $('.site-header'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
+
+
+	// Scroll To Top JS //
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100){
+			$('.gotopbtn').fadeIn(1000);
+		} 
+		else{
+			$('.gotopbtn').fadeOut(1000);
+		}
+	});
+	$('.gotopbtn').click(function(){
+		$("html, body").animate({scrollTop : 0},700);
+		return false;
+	});
+
+	// Scroll To Top JS //
+
 $(document).ready(function(){
   $('form input').change(function () {
     $('form p').text(this.files.length + " file(s) selected");
